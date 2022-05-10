353bf4738c45dbd06cdfbde8ede2a1f944c9ecc4
Estebanmual <esteban.munozal@gmail.com>
2022-05-10 13:13:58 -0500

    Add Person class

---
+++ classes.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+class Person
+    attr_accessor :name, :age
+    attr_reader :id
+
+    def initialize( age, name='Unknown', parent_permission=true)
+        @age = age
+        @name = name
+        @parent_permission = parent_permission
+        @id = rand(100000)
+    end
+
+    private
+
+    def is_of_age?
+        return @age >= 18
+    end
+
+    public
+
+    def can_use_services?
+        true if is_of_age? || @parent_permission
+    end
+end
