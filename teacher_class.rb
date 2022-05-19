b33e5dc1925418fffeb50b4baa7c9d35fb78742a
Estebanmual <esteban.munozal@gmail.com>
2022-05-19 11:36:13 -0500

    Added teacher tests

--- teacher_class.rb
+++ teacher_class.rb
@@ -1,6 +1,8 @@
 require './person_class'
 
 class Teacher < Person
+  attr_accessor :specialization
+  
   def initialize(specialization, age, name)
     super(age, name)
     @specialization = specialization
